a097d8a5d80facf8fd0b275092cb2a78735c328c
Ian Kariniemi <i.kariniemi@gmail.com>
2020-10-21 14:43:01 -0500

    Trying out Initializer productions.

--- grammars/edu.umn.cs.melt.exts.ableC.checkedc/abstractsyntax/Type.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.checkedc/abstractsyntax/Type.sv
@@ -6,7 +6,7 @@ imports silver:langutil:pp;
 imports edu:umn:cs:melt:ableC:abstractsyntax:host hiding checkedPtrType;
 imports edu:umn:cs:melt:ableC:abstractsyntax:construction;
 imports edu:umn:cs:melt:ableC:abstractsyntax:env;
-imports edu:umn:cs:melt:ableC:abstractsyntax:overloadable as ovrld;
+imports edu:umn:cs:melt:ableC:abstractsyntax:overloadable;
 --imports edu:umn:cs:melt:ableC:abstractsyntax:debug;
 
 imports edu:umn:cs:melt:exts:ableC:templating;
@@ -43,6 +43,22 @@ top::Expr ::= e1::Expr e2::Expr
 {
   top.pp = pp"${e1.pp} + ${e2.pp}";
 
+
+  local localErrors::[Message] =
+     e1.errors ++ e2.errors ++
+     [err(e1.location, s"Arithmetic on Checked pointer not allowed.")];
+
+  local fwrd::Expr = errorExpr(localErrors, location=builtin);
+
+  forwards to mkErrorCheck(localErrors, fwrd);
+}
+
+
+abstract production equalsCheckedPtr
+top::Expr ::= e1::Expr e2::Expr
+{
+  top.pp = pp"${e1.pp} + ${e2.pp}";
+
   local subType::Type = checkedPtrSubType(e1.typerep);
 
   local localErrors::[Message] =
@@ -57,7 +73,33 @@ top::Expr ::= e1::Expr e2::Expr
 }
 
 
+abstract production assignCheckedPtr
+top::Expr ::= lhs::Expr rhs::Expr
+{
+  top.pp = pp"${lhs.pp} = ${rhs.pp}";
 
+  local subType::Type = checkedPtrSubType(lhs.typerep);
+
+  local localErrors::[Message] =
+     lhs.errors ++ rhs.errors;
+
+  -- local fwrd::Expr = eqExpr(lhs, pointerExpr(rhs, location=builtin)
+  --                              ,location=builtin);
+
+  -- local fwrd::Expr = ableC_Expr {$Expr{lhs} = $Expr{rhs};};
+
+  local fwrd::Expr = eqExpr(lhs, decExpr(rhs, location=builtin), location=builtin);
+
+
+  forwards to mkErrorCheck(localErrors, fwrd);
+}
+
+
+abstract production initCheckedPtr
+top::Initializer ::= e::Expr
+{
+  forwards to exprInitializer(decExpr(e, location=builtin), location=top.location);
+}
 
 
 
@@ -65,22 +107,25 @@ abstract production checkedPtrType
 top::ExtType ::= sub::Type
 {
   propagate canonicalType;
-  top.pp = pp"checked_Ptr_${sub.pp}";
-  top.host = pointerType(top.givenQualifiers,sub);
+  top.pp = pp"checkedPtr<${sub.lpp}${sub.rpp}>";
   top.mangledName = s"checkedPtr_${sub.mangledName}_";
+  top.host = pointerType(top.givenQualifiers,sub);
   top.isEqualTo =
     \ other::ExtType ->
       case other of
-        checkedPtrType(otherSub) -> compatibleTypes(sub, otherSub, false, false)
-      | _ -> false
+        | checkedPtrType(otherSub) -> compatibleTypes(sub, otherSub, false, false)
+        | _ -> false
       end;
 
+
+   top.lEqProd = just(assignCheckedPtr(_, _, location=_));
   -- TODO: Figure out what these mean
-  -- top.lAddProd = just(addCheckedPtr(_, _, location=_));
-  -- top.rAddProd = just(addCheckedPtr(_, _, location=_));
+   top.lAddProd = just(addCheckedPtr(_, _, location=_));
+   top.rAddProd = just(addCheckedPtr(_, _, location=_));
   -- Overload for += automatically inferred from above
-  -- top.lEqualsProd = just(equalsVector(_, _, location=_));
-  -- top.rEqualsProd = just(equalsVector(_, _, location=_));
+   top.lEqualsProd = just(equalsCheckedPtr(_, _, location=_));
+   top.rEqualsProd = just(equalsCheckedPtr(_, _, location=_));
+   top.exprInitProd = just(initCheckedPtr(_, location=_));
   -- Overload for != automatically inferred from above
   -- top.addressOfArraySubscriptProd = just(addressOfSubscriptVector(_, _, location=_));
   -- Overloads for [], []= automatically inferred from above
